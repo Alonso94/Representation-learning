d02dbdfdb92524bcab3f1b0e202bd33f372864ad
Alonso94 <ay20-5-1994@hotmail.com>
2020-04-05 11:25:58 +0300

    File I/O - to solve RAM problem for multi-view

--- TCN.py
+++ TCN.py
@@ -64,93 +64,6 @@ class TCN(nn.Module):
         x=self.fc7(x.view(x.size()[0],-1))
         return self.normalize(x)*self.alpha
 
-class TripletBuilder:
-    def __init__(self):
-        print("Triplet builder started")
-        self.positive_margin=10
-        self.negative_margin=30
-        self.video_index=0
-
-        # read video directory
-        self.path="/home/ali/Representation-learning/videos"
-        filenames=[p for p in os.listdir(self.path) if p[0]!='.']
-        self.video_paths=[os.path.join(self.path,f) for f in filenames]
-        self.video_count=len(self.video_paths)
-        # count frames
-        self.frame_lengths = np.array([len(imageio.mimread(p)) for p in self.video_paths])
-        self.cum_length = np.cumsum(self.frame_lengths)
-        # logging
-        print("The number fo the videos:", self.video_count)
-        print(" video pathes:")
-        for i in range(self.video_count):
-            print("%d. %s - %d frames" % (i, self.video_paths[i],self.frame_lengths[i]))
-        self.frame_size=(640,320)
-
-    # Decorator to wrap a function with a memorizing callable that saves up to the maxsize most recent calls.
-    # It can save time when an expensive or I/O bound function is periodically called with the same arguments.
-    @functools.lru_cache(maxsize=1)
-    def get_video(self,index):
-        video_path=self.video_paths[index]
-        print("reading file")
-        video=imageio.mimread(video_path)
-        frames=np.zeros((self.frame_lengths[index]+1,3,*self.frame_size))
-        i=0
-        for frame in video:
-            image=Image.fromarray(frame)
-            image=image.resize(self.frame_size)
-            scaled=np.array(image,dtype=np.float32)/255
-            tr_img=np.transpose(scaled,[2,1,0])
-            frames[i,:,:,:]=tr_img
-            i+=1
-        print("frames collected")
-        return frames
-
-    def sample_anchor(self):
-        return np.random.choice(np.arange(self.frame_lengths[self.video_index]))
-
-    def sample_positive(self,anchor):
-        min_=max(0,anchor-self.positive_margin)
-        max_=min(self.frame_lengths[self.video_index]-1,anchor+self.positive_margin)
-        return np.random.choice(np.arange(min_,max_))
-
-    def sample_negative(self,anchor):
-        end1=max(0,anchor-self.negative_margin)
-        range1=np.arange(0,end1)
-        start2=min(self.frame_lengths[self.video_index]-1,anchor+self.negative_margin)
-        range2=np.arange(start2,self.frame_lengths[self.video_index])
-        range=np.concatenate([range1,range2])
-        return np.random.choice(range)
-
-    def sample_triplet(self,frames):
-        anchor_index=self.sample_anchor()
-        # print("anchor index",anchor_index)
-        anchor=frames[anchor_index]
-        pos=frames[self.sample_positive(anchor_index)]
-        neg=frames[self.sample_negative(anchor_index)]
-        # print("took samples")
-        anchor=torch.from_numpy(anchor).to(device)
-        # print("anchor tensor")
-        pos=torch.from_numpy(pos).to(device)
-        # print("pos tensor")
-        neg=torch.from_numpy(neg).to(device)
-        # print("neg tensor")
-        return (anchor,pos,neg)
-
-    def collect_triplets(self,sample_size=200):
-        triplets=torch.FloatTensor(sample_size,3,3,*(self.frame_size)).to(device)
-        frames = self.get_video(self.video_index)
-        for i in range(sample_size):
-            # print(i, "collect triplets")
-            anchor,pos,neg=self.sample_triplet(frames)
-            # print("after sample triplet",anchor.shape)
-            triplets.data[i,0,:,:,:]=anchor.data
-            triplets.data[i,1,:,:,:]=pos.data
-            triplets.data[i,2,:,:,:]=neg.data
-            # print("iteration %d " % i,triplets.shape)
-        self.video_index=(self.video_index+1)%self.video_count
-        # print(triplets.shape, "collect triplets")
-        return torch.utils.data.TensorDataset(triplets,torch.zeros(triplets.size()[0]))
-
 class TCN_trainer:
     def __init__(self):
         print("TCN trainer started")
@@ -169,7 +82,6 @@ class TCN_trainer:
         self.queue=multiprocessing.Queue(1)
         self.dataset_builder_process=multiprocessing.Process(target=self.build_set,args=(self.queue,))#,daemon=True)
         self.dataset_builder_process.start()
-        #TODO: collect the dataset before training - RAM problems
 
     def build_set(self,queue=None):
         print("\n start process")
